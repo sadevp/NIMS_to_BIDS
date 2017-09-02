c2e27f47666427ae50719a606143091e604e37ae
Natalia Vélez <nvelez@stanford.edu>
2017-09-01 21:55:52 -0700

    converted NIMS_to_BIDS to Sherlock file structure

--- NIMS_to_BIDS.py
+++ NIMS_to_BIDS.py
@@ -29,11 +29,11 @@ home_dir = os.environ['PI_HOME']
 
 #Get Data Filepath
 if (len(sys.argv) == 2):
-    project_dir = str(sys.argv[1]).strip(' ')
+	project_dir = str(sys.argv[1]).strip(' ')
 else:
-    #"Data needs to be in format: \n       Project Filename            \n        /          \\              \n    NIMS_data  BIDS_info.xlsx      \n       /                           \nSub1 Sub2 Sub3                     \n\n                                   \n
-    print("NIMS_to_BIDS.py can take the project's file path as an argument\nNo argument detected\nPlease drag in file path from folder")
-    project_dir = input().strip(' ')
+	#"Data needs to be in format: \n       Project Filename            \n        /          \\              \n    NIMS_data  BIDS_info.xlsx      \n       /                           \nSub1 Sub2 Sub3                     \n\n                                   \n
+	print("NIMS_to_BIDS.py can take the project's file path as an argument\nNo argument detected\nPlease drag in file path from folder")
+	project_dir = input().strip(' ')
 
 project_filepath = opj(home_dir, project_dir)
 
@@ -55,9 +55,9 @@ xls = pd.ExcelFile(project_filepath + "/" + BIDS_filename[0])
 
 #Make folder if folder doesn't exist function
 def makefolder(name):
-    if not os.path.exists(name):
-        os.makedirs(name)
-        
+	if not os.path.exists(name):
+		os.makedirs(name)
+		
 #Load and Clean XLS File
 participants = xls.parse('participants')
 participants.participant_id = participants.participant_id.astype('str')
@@ -83,180 +83,173 @@ NIMS_protocol_filenames = protocol.NIMS_scan_title.tolist() #Convert protocol sc
 NIMS_BIDS_conversion = protocol[["NIMS_scan_title","BIDS_scan_title_path"]]
 
 
-# In[ ]:
-
 def check_against_protocol(participants,protocol): 
-    
-    all_files_correct = True
-    
-    for index, row in participants.iterrows():
+	
+	all_files_correct = True
+	
+	for index, row in participants.iterrows():
 
-        #If directory is there, try will work
-        try:
-            #Get all files in participant directory
-            NIMS_participant_filenames = opj(NIMS, row.nims_title)
-           
-            #Delete all non-nii.gz files
-            NIMS_participant_filenames = [x for x in NIMS_participant_filenames if ".nii.gz"  in x]
+		try:
+			NIMS_participant = opj(NIMS, row.nims_title)
+			NIMS_participant_subdirs = [opj(NIMS_participant, d) for d in os.listdir(NIMS_participant)]
+			NIMS_participant_subdirs = [d for d in NIMS_participant_subdirs if os.path.isdir(d)]
 
-            for item in set(NIMS_protocol_filenames):
-                
-                directory_filenames = [x for x in NIMS_participant_filenames if item in x]
-                protocol_filenames = NIMS_BIDS_conversion[NIMS_BIDS_conversion.NIMS_scan_title.str.contains(item)]
-                protocol_filenames = protocol_filenames.iloc[:,1].tolist()
+			for item in set(NIMS_protocol_filenames):
+				protocol_dirs = [d for d in NIMS_participant_subdirs if item in d]
+				directory_filenames = []
+				for d in protocol_dirs:
+					protocol_search = glob.glob(opj(d, "*.nii.gz"))
+					directory_filenames.append(protocol_search[0])
 
-                if len(directory_filenames) < len(protocol_filenames):
-                    print('{} : sub-{} : << {} {} files in folder {} files in protocol\n'.                    format(str(row.nims_title), str(row.participant_id), item.rjust(20), len(directory_filenames), len(protocol_filenames)))
+				protocol_filenames = NIMS_BIDS_conversion[NIMS_BIDS_conversion.NIMS_scan_title.str.contains(item)]
+				protocol_filenames = protocol_filenames.iloc[:,1].tolist()
 
-                elif len(directory_filenames) > len(protocol_filenames):
-                    print('{} : sub-{} : >> {} {} files in folder {} files in protocol\n'.                    format(str(row.nims_title), str(row.participant_id), item.rjust(20), len(directory_filenames), len(protocol_filenames)))
-                    all_files_correct = False
-                    
-                elif len(directory_filenames) == len(protocol_filenames):
-                    print('{} : sub-{} : == {} {} files in folder {} files in protocol\n'.                    format(str(row.nims_title), str(row.participant_id), item.rjust(20), len(directory_filenames), len(protocol_filenames)))
+				if len(directory_filenames) < len(protocol_filenames):
+					print('{} : sub-{} : << {} {} files in folder {} files in protocol\n'.                    format(str(row.nims_title), str(row.participant_id), item.rjust(20), len(directory_filenames), len(protocol_filenames)))
 
-            print("------------")
-        
-        except:
-            all_files_correct = False
-            print("sub-" + str(row.participant_id) + " : -- ERROR - folder is missing \n------------")
+				elif len(directory_filenames) > len(protocol_filenames):
+					print('{} : sub-{} : >> {} {} files in folder {} files in protocol\n'.                    format(str(row.nims_title), str(row.participant_id), item.rjust(20), len(directory_filenames), len(protocol_filenames)))
+					all_files_correct = False
+					
+				elif len(directory_filenames) == len(protocol_filenames):
+					print('{} : sub-{} : == {} {} files in folder {} files in protocol\n'.                    format(str(row.nims_title), str(row.participant_id), item.rjust(20), len(directory_filenames), len(protocol_filenames)))
 
-        
-        
-        
-    if all_files_correct:
-        print("\nAll your folders match your protocol\n")  
-    else:
-        print("\nSome folders do not match your protocol, please resolve errors\n")
-    
-    return all_files_correct
+			print("------------")
+		
+		except:
+			all_files_correct = False
+			print("sub-" + str(row.participant_id) + " : -- ERROR - folder is missing \n------------")
+
+
+	return all_files_correct
 
 
 # In[ ]:
 
 def write_text_files(participants, protocol): 
-    
-    def to_file(filename, content): 
-        with open(BIDS + filename + ".json", "w") as text_file:
-            text_file.write(content)
-    
-    #Data Description
-    dataset_description = json.dumps({"BIDSVersion": "1.0.0",                                    "License": "",                                    "Name": "dummy task name",                                   "ReferencesAndLinks": ""})
-    to_file(str("dataset_description"), str(dataset_description))
-    
+	
+	def to_file(filename, content): 
+		with open(BIDS + filename + ".json", "w") as text_file:
+			text_file.write(content)
+	
+	#Data Description
+	dataset_description = json.dumps({"BIDSVersion": "1.0.0",                                    "License": "",                                    "Name": "dummy task name",                                   "ReferencesAndLinks": ""})
+	to_file(str("dataset_description"), str(dataset_description))
+	
 
-    #Task Description
-    for item in set(protocol.loc[protocol.ANAT_or_FUNC == "func", 'BIDS_scan_title']):
-        full_task_name = protocol.loc[protocol.BIDS_scan_title == item, 'full_task_name']
-        full_task_name = full_task_name.reset_index(drop=True)[0] #Gets first instance of RT
-        
-        repetition_time = protocol.loc[protocol.BIDS_scan_title == item, 'repetition_time']
-        repetition_time = repetition_time.reset_index(drop=True)[0] #Gets first instance of RT
-        task_json = json.dumps({"RepetitionTime": repetition_time, "TaskName" : full_task_name})
+	#Task Description
+	for item in set(protocol.loc[protocol.ANAT_or_FUNC == "func", 'BIDS_scan_title']):
+		full_task_name = protocol.loc[protocol.BIDS_scan_title == item, 'full_task_name']
+		full_task_name = full_task_name.reset_index(drop=True)[0] #Gets first instance of RT
+		
+		repetition_time = protocol.loc[protocol.BIDS_scan_title == item, 'repetition_time']
+		repetition_time = repetition_time.reset_index(drop=True)[0] #Gets first instance of RT
+		task_json = json.dumps({"RepetitionTime": repetition_time, "TaskName" : full_task_name})
 
-        to_file(str(item + "_bold"), str(task_json))
+		to_file(str(item + "_bold"), str(task_json))
 
-    #TSV
-    participant_tsv = participants.loc[:, ['participant_id', 'sex', 'age']]
-    participant_tsv.loc[:, 'participant_id'] = "sub-" + participant_tsv.loc[:, 'participant_id'].apply(str)
-    #Had to write csv and then change it due to python 2/3 incompatability
-    participant_tsv.to_csv(BIDS + 'participants.tsv', index=False)
-    # Read in the file
-    with open(BIDS + 'participants.tsv', 'r') as file :
-        filedata = file.read()
+	#TSV
+	participant_tsv = participants.loc[:, ['participant_id', 'sex', 'age']]
+	participant_tsv.loc[:, 'participant_id'] = "sub-" + participant_tsv.loc[:, 'participant_id'].apply(str)
+	#Had to write csv and then change it due to python 2/3 incompatability
+	participant_tsv.to_csv(BIDS + 'participants.tsv', index=False)
+	# Read in the file
+	with open(BIDS + 'participants.tsv', 'r') as file :
+		filedata = file.read()
 
-    # Replace the target string
-    filedata = filedata.replace(',', '\t')
+	# Replace the target string
+	filedata = filedata.replace(',', '\t')
 
-    # Write the file out again
-    with open(BIDS + 'participants.tsv', 'w') as file:
-        file.write(filedata)
-    
-    
-    
-
-
+	# Write the file out again
+	with open(BIDS + 'participants.tsv', 'w') as file:
+		file.write(filedata)
+	
+	
 # In[ ]:
 
 def convert_to_bids(participants, protocol):
-    
-    print("Comparing Folders to Protocol...\n")
-    
-    if check_against_protocol(participants,protocol): #Function returns true is everything matches
-        
-        
-        print("Creating BIDS_data folder\n")
-        #Make BIDS Folder
-        makefolder(BIDS)
-        participants.participant_id.apply(lambda x: makefolder(BIDS + 'sub-' + str(x) + "/anat"))
-        participants.participant_id.apply(lambda x: makefolder(BIDS + 'sub-' + str(x) + "/func"))
-        
-        for index, row in participants.iterrows():
-            #Get files
-            NIMS_participant_filenames = os.listdir(NIMS + row.nims_title)
+	
+	print("Comparing Folders to Protocol...\n")
+	
+	if check_against_protocol(participants,protocol): #Function returns true is everything matches
+		
+		
+		print("Creating BIDS_data folder\n")
+		#Make BIDS Folder
+		makefolder(BIDS)
+		participants.participant_id.apply(lambda x: makefolder(BIDS + 'sub-' + str(x) + "/anat"))
+		participants.participant_id.apply(lambda x: makefolder(BIDS + 'sub-' + str(x) + "/func"))
+		
+		for index, row in participants.iterrows():
+			NIMS_participant = opj(NIMS, row.nims_title)
+			NIMS_participant_subdirs = [opj(NIMS_participant, d) for d in os.listdir(NIMS_participant)]
+			NIMS_participant_subdirs = [d for d in NIMS_participant_subdirs if os.path.isdir(d)]
 
-            #Delete all non-nii.gz files from list
-            NIMS_participant_filenames = [x for x in NIMS_participant_filenames if ".nii.gz"  in x]
+			for item in set(NIMS_protocol_filenames):
+				protocol_dirs = [d for d in NIMS_participant_subdirs if item in d]
+				directory_filenames = []
+				for d in protocol_dirs:
+					protocol_search = glob.glob(opj(d, "*.nii.gz"))
+					directory_filenames.append(protocol_search[0])
 
-            for item in set(NIMS_protocol_filenames):
-                directory_filenames = [x for x in NIMS_participant_filenames if item in x]
-                protocol_filenames = NIMS_BIDS_conversion[NIMS_BIDS_conversion.NIMS_scan_title.str.contains(item)]
-                protocol_filenames = protocol_filenames.iloc[:,1].tolist()
+				protocol_filenames = NIMS_BIDS_conversion[NIMS_BIDS_conversion.NIMS_scan_title.str.contains(item)]
+				protocol_filenames = protocol_filenames.iloc[:,1].tolist()
 
-                for index, item in enumerate(directory_filenames):
-                    oldpath = (NIMS + row.nims_title + "/" + directory_filenames[index])
-                    newpath = (protocol_filenames[index].replace("###", str(row.participant_id)))
-                    copyfile(oldpath, newpath)
+				for index, item in enumerate(directory_filenames):
+					oldpath = (NIMS + row.nims_title + "/" + directory_filenames[index])
+					pdb.set_trace()
+					newpath = (protocol_filenames[index].replace("###", str(row.participant_id)))
+					copyfile(oldpath, newpath)
 
-                    print("sub-" + str(row.participant_id) + ": ++ "+ os.path.basename(newpath).rjust(20))
-            print("------------")
+					print("sub-" + str(row.participant_id) + ": ++ "+ os.path.basename(newpath).rjust(20))
+			print("------------")
 
-        print("\nCreating JSON and .tsv Files")
-        
-        write_text_files(participants, protocol)
-       
-        print("\nDone!")
+		print("\nCreating JSON and .tsv Files")
+		
+		write_text_files(participants, protocol)
+	   
+		print("\nDone!")
 
 
 # In[ ]:
 
 def reorient_and_skullstrip(participants, protocol):
-    
-    print("Comparing Folders to Protocol...\n")
-    
-    if check_against_protocol(participants,protocol): #Function returns true is everything matches
-        
-        print("Creating BIDS_data folder\n")
-        #Make BIDS Folder
-        makefolder(BIDS)
-        participants.participant_id.apply(lambda x: makefolder(BIDS + 'sub-' + str(x) + "/anat"))
-        participants.participant_id.apply(lambda x: makefolder(BIDS + 'sub-' + str(x) + "/func"))
-        
-        for index, row in participants.iterrows():
-            #Get files
-            NIMS_participant_filenames = os.listdir(NIMS + row.nims_title)
+	
+	print("Comparing Folders to Protocol...\n")
+	
+	if check_against_protocol(participants,protocol): #Function returns true is everything matches
+		
+		print("Creating BIDS_data folder\n")
+		#Make BIDS Folder
+		makefolder(BIDS)
+		participants.participant_id.apply(lambda x: makefolder(BIDS + 'sub-' + str(x) + "/anat"))
+		participants.participant_id.apply(lambda x: makefolder(BIDS + 'sub-' + str(x) + "/func"))
+		
+		for index, row in participants.iterrows():
+			#Get files
+			NIMS_participant_filenames = os.listdir(NIMS + row.nims_title)
 
-            #Delete all non-nii.gz files from list
-            NIMS_participant_filenames = [x for x in NIMS_participant_filenames if ".nii.gz"  in x]
+			#Delete all non-nii.gz files from list
+			NIMS_participant_filenames = [x for x in NIMS_participant_filenames if ".nii.gz"  in x]
 
-            for item in set(NIMS_protocol_filenames):
-                directory_filenames = [x for x in NIMS_participant_filenames if item in x]
-                protocol_filenames = NIMS_BIDS_conversion[NIMS_BIDS_conversion.NIMS_scan_title.str.contains(item)]
-                protocol_filenames = protocol_filenames.iloc[:,1].tolist()
+			for item in set(NIMS_protocol_filenames):
+				directory_filenames = [x for x in NIMS_participant_filenames if item in x]
+				protocol_filenames = NIMS_BIDS_conversion[NIMS_BIDS_conversion.NIMS_scan_title.str.contains(item)]
+				protocol_filenames = protocol_filenames.iloc[:,1].tolist()
 
-                for index, item in enumerate(directory_filenames):
-                    oldpath = (NIMS + row.nims_title + "/" + directory_filenames[index])
-                    newpath = (protocol_filenames[index].replace("###", str(row.participant_id)))
-                    copyfile(oldpath, newpath)
+				for index, item in enumerate(directory_filenames):
+					oldpath = (NIMS + row.nims_title + "/" + directory_filenames[index])
+					newpath = (protocol_filenames[index].replace("###", str(row.participant_id)))
+					copyfile(oldpath, newpath)
 
-                    print("sub-" + str(row.participant_id) + ": ++ "+ os.path.basename(newpath))
-            print("------------")
+					print("sub-" + str(row.participant_id) + ": ++ "+ os.path.basename(newpath))
+			print("------------")
 
-        print("\nCreating JSON and .tsv Files")
-        
-        write_text_files(participants, protocol)
-       
-        print("\nDone!")
+		print("\nCreating JSON and .tsv Files")
+		
+		write_text_files(participants, protocol)
+	   
+		print("\nDone!")
 
 
 # In[ ]:
