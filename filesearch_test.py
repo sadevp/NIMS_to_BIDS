fcaacdc30152898b4e602b80a654469f57bdafe5
Natalia Vélez <nvelez@stanford.edu>
2017-09-01 20:46:45 -0700

    search for eligible directories

--- filesearch_test.py
+++ filesearch_test.py
@@ -92,7 +92,9 @@ def check_against_protocol(participants,protocol):
     	NIMS_participant_subdirs = [opj(NIMS_participant, d) for d in os.listdir(NIMS_participant)]
     	NIMS_participant_subdirs = [d for d in NIMS_participant_subdirs if os.path.isdir(d)]
 
-    	print(NIMS_participant_subdirs)
+        for item in set(NIMS_protocol_filenames):
+            protocol_dirs = [d for d in NIMS_participant_subdirs if item in d]
+            print protocol_dirs
 
 
         # #If directory is there, try will work
