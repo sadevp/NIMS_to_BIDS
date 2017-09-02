ddc5bc295f117c2b754879c3f82659fcf0b97e66
Natalia Andrea Velez Alicea <nvelez@sherlock-ln01.stanford.edu>
2017-09-01 20:12:17 -0700

    minor change to fieldmap_test

--- fieldmap_test.py
+++ fieldmap_test.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 from __future__ import absolute_import
-from __future__ import standard_library
+from future import standard_library
 
 # coding: utf-8
 
