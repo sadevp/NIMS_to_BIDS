5d8f5aef8ff92431c711b3d54e736dd7529d8e49
Natalia Vélez <nvelez@stanford.edu>
2017-09-01 19:11:20 -0700

    error importing future (take 2)

--- fieldmap_test.py
+++ fieldmap_test.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 from __future__ import absolute_import
+from __future__ import standard_library
 
 # coding: utf-8
 
@@ -10,7 +11,6 @@ from __future__ import absolute_import
 from builtins import input
 from builtins import open
 from builtins import str
-from __future__ import standard_library
 standard_library.install_aliases()
 print("Importing Libraries...\n")
 
