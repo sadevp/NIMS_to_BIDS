a5f6857d8306287013b2aefe27618c6c3b1be60c
Natalia Vélez <nvelez@stanford.edu>
2017-09-01 21:59:02 -0700

    debugging...

--- NIMS_to_BIDS.py
+++ NIMS_to_BIDS.py
@@ -93,7 +93,7 @@ def check_against_protocol(participants,protocol):
 			NIMS_participant = opj(NIMS, row.nims_title)
 			NIMS_participant_subdirs = [opj(NIMS_participant, d) for d in os.listdir(NIMS_participant)]
 			NIMS_participant_subdirs = [d for d in NIMS_participant_subdirs if os.path.isdir(d)]
-			print(NIMS_participant)
+			print(NIMS_participant_subdirs)
 
 			for item in set(NIMS_protocol_filenames):
 				protocol_dirs = [d for d in NIMS_participant_subdirs if item in d]
